4bc9a2aaa728c44750aec6f1a4ad1381967e7b30
elhambahman <elhambahmangml@gmail.com>
2023-07-02 11:08:29 +0330

    lattery

--- Lattery.sol
+++ Lattery.sol
@@ -1,26 +1,41 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity 0.8.18;
+pragma solidity 0.8.18 ;
+contract Lattery{
+     event lattery(address manager , address [] players , uint value , bytes data);
+     mapping(uint => address)public Winner;
+     address public manager;
+     address payable []public players;
+     uint value ;
 
- contract L2{
-    event l2(address manager , address payabls , uint value);
-    address public manager;
-    address payable[]public players;
-    address public winner;
-    uint amount;
-    mapping(uint => address)public Winner;
-    constructor(){
-          manager = msg.sender;
+  
+constructor(){
+    manager = msg.sender;
+}
+//ثبت نام در لاتاری
+function register(uint amount)public payable returns(bool){
+    //1اتر واریز کنه
+    amount = 1 ether;
+     require(msg.value > 1 ether);
+     //اضافه اش کن به شرکت کنندگان
+     players.push(payable(msg.sender));
+     return true;
     }
-    function l21( uint _amount)public payable returns(bool){
-         amount = _amount;
-         require(msg.value > 1 ether);
-         players.push(payable(msg.sender));
-         return true;
-             }
-
+//انتخاب به صورت تصادفی
     function random()private view returns(uint){
-                
-     }
-
- }
+      uint(keccak256(abi.encodePacked(block.timestamp,block.difficulty,msg.sender))) % 50000;
+      abi.encodePacked(block.timestamp,block.difficulty,players.length);
+        } 
+//تعیین شماره برنده 
+    function pickWinner()public {
+     uint index = random() % players.length;
+      players[index].transfer(address(this).balance);
+      players = new address payable[](0);
+     
+    }
+    
+    function getplayers()public view returns(address payable[]memory){
+      return players;
+    }
+     
+}
