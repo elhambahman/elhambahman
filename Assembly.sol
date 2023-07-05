14aa8bc6ad8142869121734eb18ae63f6ea1e75d
elhambahman <elhambahmangml@gmail.com>
2023-07-05 14:54:24 +0330

    start project

--- Assembly.sol
+++ Assembly.sol
@@ -1,60 +1,64 @@
 // SPDX-License-Identifier: MIT
  pragma solidity 0.8.18 ;
  contract Newlaw{
-     event voting(uint totalvotes , uint agree , uint Against , uint havenoopinion , uint void);
+     event voting(uint totalvotes , uint agree , uint Against , uint noidea , uint void);
      event time(uint timestampstart ,uint  TimeStampEnd , string  namelaw);
 
      mapping(address => bool)public isrepresentatives;
      address public representatives;
-     address[]public Chairman;   
-     uint Against ; 
-     uint agree ; 
-     uint noidea ; 
-     uint void ;
-     uint timestampstart ;
-     uint TimeStampEnd;
-     string namelaw;
-     string Nocomment;
-     string  _Noidea = Nocomment ;
-     bool Agree = true ;
-     bool against = false;
+     address[]public Chairman; 
 
-      
       struct newlaw{
           uint timestampstart ; 
           uint totalvotes ;
           uint agree ;
           uint Against ;
-          uint havenoopinion ; 
+          uint noidea ; 
           uint void ;
           uint TimeStampEnd;
       }
+     newlaw[] Laws ;
 
-     modifier Conditions(){
+     modifier onlyRepresentative(){
         require(msg.sender == representatives , "Representatives only have the right to vote");
          _;
          require(!isrepresentatives[msg.sender] , "You have already voted");
          _;
      }
-     modifier timestamp{
-         require(block.timestamp < timestampstart + 10 minutes);
+
+     modifier validTimestamp{
+         require(block.timestamp < Laws.timestampstart + 10 minutes);
          _;
      }
+
+     function addlow(uint _timestampstart ,  uint _totalvotes, uint _Against , uint _agree ,uint _noidea ,uint _void  ,uint _TimeStampEnd)public{
+         Laws.push(newlaw({timestampstart : _timestampstart ,totalvotes : _totalvotes, Against : _Against , agree : _agree , noidea : _noidea , void : _void ,TimeStampEnd : _TimeStampEnd }));
+     }
     
-    function Voting(bool vote  , string memory Noidea )public returns(uint , uint , uint , uint){
-        if(vote = Agree){
-            agree++ ;
+    function hasRepresentatives(uint vote)public returns(uint ,uint , uint , uint , uint){
+        if(vote == Laws.agree){
+            Laws.agree++ ;
         }
-        if(vote = against){
-            Against++;
+        if(vote == Laws.Against){
+           Laws. Against++;
         }
-        if( Noidea == _Noidea ){
-        noidea++ ;
+        if( vote == Laws.noidea ){
+       Laws.noidea++ ;
         }
         else{
-            void++ ;
+           Laws.void++ ;
         }
-        return(agree , Against, noidea , void);
+      
+       Laws.totalvotes ++;
+        emit voting(Laws.totalvotes, Laws.agree, Laws.Against, Laws.noidea, Laws.void);
+        return(Laws.totalvotes, Laws.agree , Laws.Against, Laws.noidea , Laws.void);
         }
- 
+
+
+ function endOfVoting()public returns(uint _timestampstart, uint _totalvotes , uint _agree , uint _Against, uint _noidea , uint _void, uint _TimeStampEnd){
+      Laws.TimeStampEnd = block.timestamp ;
+      return(Laws.timestampstart , Laws.totalvotes , Laws.agree , Laws.Against , Laws.noidea , Laws.void , Laws.TimeStampEnd );
+
+ }
+
  }
