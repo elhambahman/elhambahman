41ffe6158225ee7e141c3011f56a8188a7cd8833
elhambahman <elhambahmangml@gmail.com>
2023-07-08 14:56:03 +0330

    work

--- mycontracts.sol
+++ mycontracts.sol
@@ -3,17 +3,16 @@ pragma solidity ^0.8.18;
  import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/IERC20.sol";
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol";
 
-contract MyToken is ERC20 {
+contract MyToken  {
         IERC20 public token ;
 
 
-    constructor(address tokenAddress)ERC20("My Token", "MTK") {
-         _mint(msg.sender, 1000000 * 10 ** decimals());
+    constructor(address tokenAddress) {
          token = IERC20(tokenAddress);
 
     }
-
-    function mint(address account, uint256 amount) public{
-        _mint(account , amount);
-    }
+function getTotalSupply()public view returns(uint){
+  return token.totalsupply();
+}
+  
 }
