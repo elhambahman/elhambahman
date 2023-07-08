3843c4179b0445a756f35d08314db38114a84cfe
elhambahman <elhambahmangml@gmail.com>
2023-07-08 12:12:50 +0330

    Smart contract working with token

--- mycontracts.sol
+++ mycontracts.sol
@@ -4,11 +4,16 @@ pragma solidity ^0.8.18;
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol";
 
 contract MyToken is ERC20 {
-    constructor() ERC20("My Token", "MTK") {
-        _mint(msg.sender, 1000000 * 10 ** decimals());
+        IERC20 public token ;
+
+
+    constructor(address tokenAddress)ERC20("My Token", "MTK") {
+         _mint(msg.sender, 1000000 * 10 ** decimals());
+         token = IERC20(tokenAddress);
+
     }
 
-    function mint(address account, uint256 amount) public {
-        _mint(account, amount);
+    function mint(address account, uint256 amount) public{
+        _mint(account , amount);
     }
 }
