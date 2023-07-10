0aa83f865e2a35f7524946174820836f2f86ca41
elhambahman <elhambahmangml@gmail.com>
2023-07-10 10:24:26 +0330

    Work with tokens

--- mycontracts.sol
+++ mycontracts.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.18;
+pragma solidity ^0.8.19;
  import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/IERC20.sol";
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol";
 
@@ -11,11 +11,31 @@ contract MyToken  {
          token = IERC20(tokenAddress);
 
     }
+  
+
 function getTotalSupply()public view returns(uint){
   return token.totalSupply();
 }
-   function getbalanceOf(address account)public view returns (uint){ 
+
+    function getbalanceOf(address account)public view returns (uint){ 
     return token.balanceOf(account);
 
     }
+    function  gettransfer(address recipient, uint amount)public  returns(bool){
+     token.transfer(recipient , amount );
+     return true; 
+    }
+
+    function getallowance(address owner, address spender) public view returns (uint){
+          return token.allowance(owner,spender);
+
+    }
+    function getapprove(address spender, uint amount) public returns (bool){
+      return token.approve(spender , amount);
+    }
+    function getmint(address tokenAddress , uint amount)public view returns(uint){
+       return  token.mint(amount);
+    }
+
+
 }
