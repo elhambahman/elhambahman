e305890214695794836a88766307c2f42ab7fa62
elhambahman <elhambahmangml@gmail.com>
2023-07-10 11:45:45 +0330

    Work with tokens

--- mycontracts.sol
+++ mycontracts.sol
@@ -1,8 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
- import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/IERC20.sol";
-import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol";
-
+ import "./IERC20.sol";
 contract MyToken  {
         IERC20 public token ;
 
