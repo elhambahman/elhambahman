7b2cb082e7fe438ab1203740a4e1795266e26e2d
elhambahman <elhambahmangml@gmail.com>
2023-07-10 11:52:15 +0330

    Work with tokens

--- IERC20.sol
+++ IERC20.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.8.18;
 
 // https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v3.0.0/contracts/token/ERC20/IERC20.sol
 interface IERC20 {
+
     function totalSupply() external view returns (uint);
 
     function balanceOf(address account) external view returns (uint);
@@ -13,7 +14,7 @@ interface IERC20 {
     function allowance(address owner, address spender) external view returns (uint);
 
     function approve(address spender, uint amount) external returns (bool);
-
+    function mint(address tokenAddress , uint amount)external returns(uint);
     function transferFrom(
         address sender,
         address recipient,
@@ -22,4 +23,13 @@ interface IERC20 {
 
     event Transfer(address indexed from, address indexed to, uint value);
     event Approval(address indexed owner, address indexed spender, uint value);
+}
+contract MyToken {
+     uint256 private _totalSupply;
+    mapping(address => uint256) private _balances;
+    function mint(address account, uint256 amount) public returns (bool) {
+        _totalSupply += amount;
+        _balances[account] += amount;
+        return true;
+    }
 }
